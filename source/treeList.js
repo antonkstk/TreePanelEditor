6603dface4b936b56d0fb334004c3686b528e64b
Anton Kos <antos94@gmail.com>
2016-02-15 08:12:07 +0200

    version with reactjs

--- source/treeList.js
+++ source/treeList.js
@@ -1,65 +1,67 @@
-var tree = document.getElementById('treeview');
-var ul = document.createElement('ul');
-var obj = {
-	director: {
-		level: 1, 
-		persons: new Array()
-	},
-	deputy: {
-		level: 2, 
-		persons: new Array()
-	},
-	contractor: {
-		level: 3, 
-		persons: new Array()
-	},
-	courier: {
-		level: 4, 
-		persons: new Array() 
-	}
-};
 var socket = io.connect('http://localhost:8080');
 socket.on('firstcall', function(data) {
-	console.log('notification received');
-	var treeNode = data;
-	buildTree(treeNode, tree);
+	console.log('json data received');
+	//var treeNode = data;
+	//buildTree(treeNode, tree);
+	buildTree(data);
 	//unsubscribe from first json upload listener
 	socket.removeAllListeners('firstcall');
 });
 
-function buildTree(treeNode, domElement) {
-    domElement.appendChild(ul);
-    
-    for (i in treeNode) {
-        var li = document.createElement('li');
-        li.innerHTML = i;
-        
-        var ulChild = document.createElement('ul');
-        for (j in treeNode[i].persons){
-        	var liChild = document.createElement('li');
-        	liChild.innerHTML = '<img onclick="moveEl(event, 1)" class="moveUp" src="../resources/images/stockIndexUp.png" /> ' + treeNode[i].persons[j] + ' <img onclick="moveEl(event, -1)" class="moveDown" src="../resources/images/stockIndexDown.png" />';
-        	ulChild.appendChild(liChild);
-        }
-        //ulChild.setAttribute("class", "childUl");
-        li.setAttribute("class", treeNode[i].level);
-        li.appendChild(ulChild);
-        ul.appendChild(li);
-    }
+function getPositions(data) {
+	var positions = new Array();
+	for(var i in data){
+		positions.push(i)
+	}
+	return positions;
 }
 
-function moveEl(event, direction) {
-	console.log("Event type: " + event.type + " Direction: "+ direction);
+function buildTree(data) {
+	var ChildUl = React.createClass({
+		render: function() {
+			var self = this;
+			return (
+				<ul>
+					{this.props.persons.map(function(person){
+						return <li key={person} className={self.props.level}> <img onClick={moveElement.bind(null, self, 1)} className="moveUp" src="../resources/images/stockIndexUp.png" onmouseover="" style={{cursor: 'pointer'}}/>
+						{person} <img onClick={moveElement.bind(null, self, -1)} className="moveDown" src="../resources/images/stockIndexDown.png" onmouseover="" style={{cursor: 'pointer'}}/></li>
+					})}
+				</ul>
+			);
+		}
+	});
 	
-	var currentEl = event.target.parentElement;
-	var parentNode = currentEl.parentNode.parentNode;
+	var ParentUl = React.createClass({
+	  render: function() {
+	    return (
+	      	<ul>
+	      		{getPositions(data).map(function(position){
+					return <li key={position}> {position} <ChildUl persons={data[position].persons} level={data[position].level}/> </li>
+				})}	
+	      	</ul>
+	    );
+	  }
+	});
+
+	ReactDOM.render(
+		<ParentUl />,
+		document.getElementById('treeview')
+	);
+}
+
+var moveElement = function (el, direction) {
+	//console.log("Event type: " + event.type + " Direction: "+ direction);
+	
+	var currentEl = ReactDOM.findDOMNode(el);
+	var parentNode = ReactDOM.findDOMNode(el).parentNode;
 	var parentNodeClassName = parentNode.className;
 	
 	if(direction==1 && parentNodeClassName != "1"){
-		currentEl.parentNode.appendChild(parentNode.previousSibling.firstElementChild.firstElementChild);
+		ReactDOM.findDOMNode(el).appendChild(parentNode.previousSibling.firstElementChild.firstElementChild);
 		parentNode.previousSibling.firstElementChild.appendChild(currentEl);	
 	}
 	if(direction==-1 && parentNodeClassName != "4"){
-		currentEl.parentNode.appendChild(parentNode.nextSibling.firstElementChild.firstElementChild);
+		ReactDOM.findDOMNode(el).appendChild(parentNode.nextSibling.firstElementChild.firstElementChild);
 		parentNode.nextSibling.firstElementChild.appendChild(currentEl);	
 	}
 	loopOverChildren(ul);
